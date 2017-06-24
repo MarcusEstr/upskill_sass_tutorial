f92e15704c4034da934c31bf425a43b68c267a4f
Marcus Estrada <estrada.marcus@gmail.com>
2017-06-24 04:40:05 +0000

    Added form validations to model.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ActiveRecord::Base
-    
+   #validation of contact form
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :comments, presence: true
 end
