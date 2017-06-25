24a65a6d2421095daad87ea173e89a088b76e82d
Marcus Estrada <estrada.marcus@gmail.com>
2017-06-25 04:16:52 +0000

    Added plan variable and parameters to home page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
+ #GET request for / which is our home page
   def home
+    #Instance variables
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
   end
   
   def about
